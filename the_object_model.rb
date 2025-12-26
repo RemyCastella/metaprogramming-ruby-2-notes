4cfbd340896f301a08357ecc4d8d8ba4c8145488
r-castella@ruby-dev.jp <r-castella@ruby-dev.jp>
2025-12-26 14:57:08 +0900

    finish Monday: The Object Model

--- the_object_model.rb
+++ the_object_model.rb
@@ -17,7 +17,7 @@ p "$%^This is an alphanumeric string(*&*)".to_alphanumeric
 # Therefore, classes can always be reopened and modified!
 
 # An object is a bunch of instance variables + a link to a class
-# A class is an object + instance methods + a link to a superclass
+# A class is an object + instance methods + a link to a super class
 
 # Objects of the same class can have different instance variables
 
@@ -62,3 +62,73 @@ p M::Const
 p M::C::Const
 p M.constants # like ls, returns all constants in M scope
 p Module.constants.grep(/\AM/) # returns all constants in top-level scope
+
+# Module names and class names can clash!
+
+module Cat
+end
+
+# class Cat <- This will throw a TypeError!
+# end
+
+# If we also want a class Cat, we need to namespace it (Namespace::Cat)
+module Namespace
+    class Cat
+    end
+end
+
+p Object.class
+p Module.superclass
+p Class.class
+p Module.superclass.class.superclass.class.superclass #there are some self-referential relationships
+
+# Ruby does two things when you call a method
+# 1. Method lookup
+# 2. Method execution (on self, the receiver)
+
+# A module can only appear once in the same chain of ancestors
+module M1
+end
+
+module M2
+    include M1
+end
+
+p M2.ancestors
+
+module M3
+    prepend M1
+    include M2 # the second inclusion of M1 within M2 is ignored
+end
+
+p M3.ancestors
+
+# Opening Kernel to write methods callable from anywhere
+
+module Kernel
+    def greet
+        system("echo hello")
+    end
+end
+
+greet
+
+# Refinements for local monkeypatching
+
+module StringExtensions
+    refine String do
+        def hex
+            gsub(/[A-Fa-f]/, "6")
+        end
+    end
+end
+
+p "ff".hex
+
+class A
+    using StringExtensions
+    p "ff".hex #our hex method is only changed in the scope where our refinement was introduced
+end
+
+p "ff".hex #now we're back out of class A so #hex is back to normal
+
