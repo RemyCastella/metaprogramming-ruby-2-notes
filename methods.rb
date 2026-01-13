2cf55543671517fef313ae5f46f115a8561143aa
r-castella@ruby-dev.jp <r-castella@ruby-dev.jp>
2026-01-13 17:40:07 +0900

    Jan 13, 2025

--- methods.rb
+++ methods.rb
@@ -59,6 +59,14 @@ class MockDataSource
     80
   end
 
+  def get_display_info(id)
+    "Sony Xperia 42"
+  end
+
+  def get_display_price(id)
+    320
+  end
+
   #...
 end
 
@@ -128,13 +136,13 @@ end
 
 # Finally we can also use dynamic method definition to get rid of the repeated definitions
 
-class Computer
+class DynamicMethodComputer
     def initialize(id, data_source)
         @id = id,
         @data_source = data_source
         data_source.methods.grep(/^get_(?<component>.*)_info$/) do
             match_data = Regexp.last_match
-            Computer.define_component(match_data[:component])
+            DynamicMethodComputer.define_component(match_data[:component])
             #this allows use to remove the define_component calls
             #is is also flexible in that new attributes to our data source will be accounted for automatically
         end
@@ -155,7 +163,7 @@ class Computer
     # define_component :keyboard
 end
 
-c = Computer.new(42, ds)
+c = DynamicMethodComputer.new(42, ds)
 puts c.cpu
 puts c.mouse
 puts c.keyboard
@@ -163,3 +171,69 @@ puts c.keyboard
 # Method missing
 
 # When we need to define many similar methods, we can use method missing to respond to calls (without defining the methods)
+
+class User
+    def method_missing(method_name, *args, &block)
+        puts "Method: #{method_name}"
+        puts "Arguments: #{args}"
+        puts "Block: #{block}" if block_given?
+        puts "Error: undefined method (#{method_name})"
+    end
+end
+
+u = User.new
+u.award_points(5){ puts "Here are some points!" }
+
+class MethodMissingComputer
+    def initialize(computer_id, data_source)
+        @id = computer_id
+        @data_source = data_source
+    end
+
+    def method_missing(method_name, *args, &block)
+        super if !@data_source.respond_to?("get_#{method_name}_info") # if its the wrong method, call BasicObject#method_missing
+        info = @data_source.send("get_#{method_name}_info", @id)
+        price = @data_source.send("get_#{method_name}_price", @id)
+        result = "#{method_name.capitalize}: #{info} ($#{price})"
+        return "* #{result}" if price >= 100
+        result
+    end
+
+    def respond_to_missing?(method_name, include_private=false) # necessary with method_missing, so that respond_to? works correctly with ghost methods
+        @data_source.respond_to?("get_#{method_name}_info") || super
+    end
+end
+
+c2 = MethodMissingComputer.new(42, ds)
+
+p c2.mouse
+p c2.keyboard
+p c2.respond_to?(:cpu) #without overwriting respond_to_missing, this would have returned false, even if the object does respond to #cpu
+
+# But what about something like :display, which is a method in Object?
+# method_missing won't be called
+
+p c2.display
+
+# There are a few ways to solve this. One way is to make MethodMissingComputer inherit from BasicObject (a blank slate)
+# Anthor way is undef_method or remove_method
+
+class MethodMissingComputer
+    undef_method :display
+end
+
+p c2.display
+
+# There is also const_missing
+
+class User
+    def self.const_missing(const_name)
+        puts "#{const_name} does not exist in #{self}"
+    end
+end
+
+User::Username
+
+# Ghost methods can be dangerous.
+# Always call super and redefine respond_to_missing
+# Dynamic Methods are safer in general (they are regular methods defined during runtime)
